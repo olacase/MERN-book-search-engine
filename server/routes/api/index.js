089ef45f6f1cac360fc0fd76f309c2e150cacb20
olacase@gmail.com <olacase@gmail.com>
2022-07-09 10:20:38 -0500

    working paths

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,14 @@
+const path = require('path')
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 
+
 router.use('/users', userRoutes);
+
+// serve react front-end in production
+router.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  });
+  
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,8 +3,25 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//import apollo server
+const { ApolloServer } = require('apollo-server-express');
+// import typeDefs and resolvers
+const { typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+
+//middleware parsing
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +31,22 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+
 app.use(routes);
+//get all
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
+const startApolloServer = async( typeDefs, resolvers)=>{
+  await server.start()
+//apply apollo server with express app
+server.applyMiddleware({ app });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
+}
+startApolloServer(typeDefs, resolvers)
